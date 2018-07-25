ea9e64bae513d7f1cd03ae0066cbad44f135cc94
romchambe <chamberomain@gmail.com>
2018-07-25 20:00:09 +0200

    add notes resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :registrations
+      resource :notes
       post '/login', to: "sessions#create"  
       post '/fb_login', to: "sessions#fb_create"
       delete '/logout', to: "sessions#delete"  
