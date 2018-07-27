d8a0b6199ab76779b20384ad2866208b01cf9bcb
romchambe <chamberomain@gmail.com>
2018-07-27 21:17:39 +0200

    add  key to note rendering in note create and index

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,13 @@
 class Note < ApplicationRecord
+
+
   has_many_attached :images
 
   belongs_to :user
+
+  def render_hash_with_attribute_key
+    note_attributes = self.attributes
+    note_attributes[:key] = 'k' + note_attributes["id"].to_s
+    return note_attributes
+  end
 end
