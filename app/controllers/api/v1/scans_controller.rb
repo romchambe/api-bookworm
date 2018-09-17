36e48413b261cdc27c7347e441c8ed4a25b1bc48
romchambe <chamberomain@gmail.com>
2018-09-17 19:04:38 +0200

    fix integrity error in upload to GCS

--- app/controllers/api/v1/scans_controller.rb
+++ app/controllers/api/v1/scans_controller.rb
@@ -9,8 +9,8 @@ module Api::V1
         filename = params[:filename]
 
         File.open("#{Rails.root}/tmp/#{filename}", 'w+b') do |file|
-          file.write(decoded_data)
-          @scan.upload.attach(io: file, filename: filename)
+          decode_file = file.write(decoded_data)
+          @scan.upload.attach(io: decoded_file, filename: filename)
         end
 
         FileUtils.rm("#{Rails.root}/tmp/images/#{filename}")
