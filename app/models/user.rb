df05e33018d04d5d3ff639de1338922bc08467a0
romchambe <chamberomain@gmail.com>
2018-11-21 14:45:54 +0100

    fix typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
-  validates :username, presence: true, length: { minimum: 2, maximum: 255 },  
+  validates :username, presence: true, length: { minimum: 2, maximum: 255 }  
   validates :email, presence: true, length: { minimum: 3, maximum: 255 },
         format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } 
   validates :password, presence: true, length: { minimum: 6 }
